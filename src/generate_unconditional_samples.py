0f6854301b8734b4da97a89aaa739cfac0f0d67f
Abstraction <31667795+wickedproblem@users.noreply.github.com>
2020-09-07 14:52:55 -0700

    this

--- src/generate_unconditional_samples.py
+++ src/generate_unconditional_samples.py
@@ -5,16 +5,13 @@ import json
 import os
 import numpy as np
 import tensorflow as tf
-import speech_recognition as sr
-import pyttsx3
-
 
 import model, sample, encoder
 
-def interact_model(
+def sample_model(
     model_name='124M',
     seed=None,
-    nsamples=1,
+    nsamples=0,
     batch_size=1,
     length=None,
     temperature=1,
@@ -23,12 +20,13 @@ def interact_model(
     models_dir='models',
 ):
     """
-    Interactively run the model
+    Run the sample_model
     :model_name=124M : String, which model to use
-    :seed=None : Integer seed for random number generators, fix seed to reproduce
-     results
-    :nsamples=1 : Number of samples to return total
-    :batch_size=1 : Number of batches (only affects speed/memory).  Must divide nsamples.
+    :seed=None : Integer seed for random number generators, fix seed to
+     reproduce results
+    :nsamples=0 : Number of samples to return, if 0, continues to
+     generate samples indefinately.
+    :batch_size=1 : Number of batches (only affects speed/memory).
     :length=None : Number of tokens in generated text, if None (default), is
      determined by model hyperparameters
     :temperature=1 : Float value controlling randomness in boltzmann
@@ -43,74 +41,39 @@ def interact_model(
      (i.e. contains the <model_name> folder)
     """
     models_dir = os.path.expanduser(os.path.expandvars(models_dir))
-    if batch_size is None:
-        batch_size = 1
-    assert nsamples % batch_size == 0
-
     enc = encoder.get_encoder(model_name, models_dir)
     hparams = model.default_hparams()
     with open(os.path.join(models_dir, model_name, 'hparams.json')) as f:
         hparams.override_from_dict(json.load(f))
 
     if length is None:
-        length = hparams.n_ctx // 2
+        length = hparams.n_ctx
     elif length > hparams.n_ctx:
         raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
 
     with tf.Session(graph=tf.Graph()) as sess:
-        context = tf.placeholder(tf.int32, [batch_size, None])
         np.random.seed(seed)
         tf.set_random_seed(seed)
+
         output = sample.sample_sequence(
             hparams=hparams, length=length,
-            context=context,
+            start_token=enc.encoder['<|endoftext|>'],
             batch_size=batch_size,
             temperature=temperature, top_k=top_k, top_p=top_p
-        )
+        )[:, 1:]
 
         saver = tf.train.Saver()
         ckpt = tf.train.latest_checkpoint(os.path.join(models_dir, model_name))
         saver.restore(sess, ckpt)
 
-        r = sr.Recognizer()
-        raw_text = ''
-
-
-
-        while True:
-            engine = pyttsx3.init()
-            engine.say("Please tell your Model Prompt")
-            engine.runAndWait()
-
-            with sr.Microphone() as source:
-                audio = r.listen(source)
-                words = r.recognize_google(audio)
-                raw_text = words
-                engine.say("You are waiting for HER to respond to ".format(raw_text))
-                engine.runAndWait()
-                # print ("You said:{}".format(raw_text))
-
-            while not raw_text:
-                print('Prompt should not be empty!')
-                raw_text = words
-
-            context_tokens = enc.encode(raw_text)
-            generated = 0
-            for _ in range(nsamples // batch_size):
-                out = sess.run(output, feed_dict={
-                    context: [context_tokens for _ in range(batch_size)]
-                })[:, len(context_tokens):]
-                for i in range(batch_size):
-                    generated += 1
-                    text = enc.decode(out[i])
-                    engine = pyttsx3.init()
-                    engine.say(text)
-                    engine.runAndWait()
-                    print("=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40)
-                    print(text)
-            print("=" * 80)
-
-
+        generated = 0
+        while nsamples == 0 or generated < nsamples:
+            out = sess.run(output)
+            for i in range(batch_size):
+                generated += batch_size
+                text = enc.decode(out[i])
+                print("=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40)
+                print(text)
 
 if __name__ == '__main__':
-    fire.Fire(interact_model)
+    fire.Fire(sample_model)
